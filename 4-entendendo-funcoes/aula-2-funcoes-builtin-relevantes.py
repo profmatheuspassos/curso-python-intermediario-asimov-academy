ee2714ac9a9a5b95f0a46ebfb055c322aa823522
Matheus Passos <40068795+profmatheuspassos@users.noreply.github.com>
2024-04-14 12:28:01 +0300

    2024-04-14

---
+++ 4-entendendo-funcoes/aula-2-funcoes-builtin-relevantes.py
@@ -0,0 +1,24 @@
+# isinstance
+print("Função isinstance - retorna o tipo da variável")
+print("valor = 2")
+valor = 2
+if isinstance(valor, int): # ou if isinstance(valor, (int, float))
+    print("Valor é int")
+else:
+    print("Não é int")
+
+print("\n")
+
+print("Funções any e all")
+booleanos = [True, False, True]
+print(all(booleanos))
+print(any(booleanos))
+
+print("\n")
+
+print("Função map")
+
+
+print("\n")
+
+print("Função filter")
